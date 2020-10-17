709278e57ce567f404e32a862aadae51c716cb1b
Dennis Kurochkin <dennis.kurochkin@gmail.com>
2020-10-17 11:17:29 +0300

    Refactor css loaders

--- webpack.config.js
+++ webpack.config.js
@@ -30,6 +30,25 @@ const optimization = () => {
 
 const filename = ext => isDev ? `[name].${ext}` : `[name].[hash].${ext}`;
 
+const cssLoaders = extra => {
+  const loaders = [ // webpack goes right to left
+    {  // compiles css to a single file
+      loader: MiniCssExtractPlugin.loader,
+      options: {
+        publicPath: ASSET_PATH,
+        hmr: isDev,
+        reloadAll: true
+      }
+    },
+    // 'style-loader', // adds styles in html head tag, has to be installed
+    'css-loader' // allows to import styles into JavaScript, has to be installed
+  ]
+
+  if (extra) loaders.push(extra);
+
+  return loaders;
+}
+
 module.exports = {
   context: path.resolve(__dirname, 'src'), // webpack working directory
   // entry: './src/index.js' // one entry point
@@ -82,34 +101,11 @@ module.exports = {
     rules: [ // rules for webpack to follow
       { // single rule
         test: /\.css$/, // files passed this test will be loaded by loaders in use[]
-        use: [ // webpack goes right to left
-          {  // compiles css to a single file
-            loader: MiniCssExtractPlugin.loader,
-            options: {
-              publicPath: ASSET_PATH,
-              hmr: isDev,
-              reloadAll: true
-            }
-          },
-          // 'style-loader', // adds styles in html head tag, has to be installed
-          'css-loader' // allows to import styles into JavaScript, has to be installed
-        ]
+        use: cssLoaders()
       },
       {
         test: /\.s[ac]ss$/,
-        use: [
-          {  // compiles css to a single file
-            loader: MiniCssExtractPlugin.loader,
-            options: {
-              publicPath: ASSET_PATH,
-              hmr: isDev,
-              reloadAll: true
-            }
-          },
-          // 'style-loader', // adds styles in html head tag, has to be installed
-          'css-loader', // allows to import styles into JavaScript, has to be installed
-          'sass-loader'
-        ]
+        use: cssLoaders('sass-loader')
       },
       {
         test: /\.(png|jpg|jpeg|svg|webp|gif)$/,
